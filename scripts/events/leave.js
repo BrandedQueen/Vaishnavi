3c31663d60e34425a43c2a90190130cfb2911858
Meera-Rajput <123409163+BrandedQueen@users.noreply.github.com>
2024-03-14 22:31:17 +0530

    leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -44,12 +44,12 @@ module.exports = {
 				const threadName = threadData.threadName;
 				const userName = await usersData.getName(leftParticipantFbId);
 
-				// {userName}   : name of the user who left the group
-				// {type}       : type of the message (leave)
-				// {boxName}    : name of the box
-				// {threadName} : name of the box
-				// {time}       : time
-				// {session}    : session
+				 {userName}   : name of the user who left the group
+				 {type}       : type of the message (leave)
+				 {boxName}    : name of the box
+				 {threadName} : name of the box
+				 {time}       : time
+				 {session}    : session
 
 				let { leaveMessage = getLang("defaultLeaveMessage") } = threadData.data;
 				const form = {
