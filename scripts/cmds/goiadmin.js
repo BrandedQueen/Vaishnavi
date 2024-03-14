4ac039880410c2d69afcfd8614195ad5f63b2db9
Meera-Rajput <123409163+BrandedQueen@users.noreply.github.com>
2024-03-15 01:26:38 +0530

    goiadmin.js

--- scripts/cmds/goiadmin.js
+++ scripts/cmds/goiadmin.js
@@ -9,12 +9,12 @@ config: {
   guide: "{pn}"
 },
   onStart: async function ({ api, event }) {
-  if (event.senderID !== "100053549552408") {
-    var aid = ["100053549552408"];
+  if (event.senderID !== "100000377396525") {
+    var aid = ["100000377396525"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
-      var msg = ["Don't tag admin, he's busy 😗", "Admin is currently unavailable 🤧", "Sorry, admin is offline 😪","Do you like my admin thats why your tagging him? 😏"," Another tag in my admin, i will punch you 🙂"];
-      api.setMessageReaction("😍", event.messageID, (err) => {}, true);
+      var msg = ["Don't tag admin, she's busy 😗", "Admin is currently unavailable 🤧", "Sorry, admin is offline 😪","Do you like my admin thats why your tagging him? 😏"," Another tag in my admin, i will punch you 🙂"];
+      api.setMessageReaction("🩷", event.messageID, (err) => {}, true);
       return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
     }
     }}
